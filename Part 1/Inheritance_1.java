3ccafe4cc86e5ca5f0d4d10cc67c4d4dac6fee18
Ashutosh Mishra <mishraashutosh60489@gmail.com>
2026-06-17 20:05:53 +0530

    Add files via upload

---
+++ Part 1/Inheritance_1.java
@@ -0,0 +1,13 @@
+public class Inheritance_1 {
+    class Animal{
+        String dog,cat,tiger;
+    }
+    class dog extends Animal{
+        String dog = "Tommy";
+    }
+
+    static void main(String[] args) {
+        Inheritance_1 a = new Inheritance_1();
+        System.out.println(a);
+    }
+}
